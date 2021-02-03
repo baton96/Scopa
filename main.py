4d4509a48b2b3c0625f78d4c30a8c1c56a19cb72
baton96 <podolce0@gmail.com>
2021-02-03 13:49:52 +0100

    Run game without form

--- main.py
+++ main.py
@@ -6,25 +6,21 @@ import uipyqt
 import scopa
 from PyQt5.QtWidgets import *
 
-# how many
-
 
 # this is the main game loop
-"""
 def run_game():
-    while sum_of_all_cards_in_game() != 0:
-        for h in range(0, no_of_players):
-            play_hand(h)
-            # print_status()
-            # check_sum_of_cards()
-
+    sc = scopa.Scopa()
+    while sc.sum_of_all_cards_in_game() != 0:
+        for h in range(scopa.no_of_players):
+            # sc.print_short_status()
+            sc.play_hand(h)
+            sc.check_sum_of_cards()
     # if any cards were left on the table, get them to the last person taking cards from the table
-    for card in table.copy():
-        piles[last_hand_played].append(card)
-        table.remove(card)
+    for card in sc.table.copy():
+        sc.piles[sc.last_hand_played].append(card)
+        sc.table.remove(card)
 
-    print_game_results()
-"""
+    sc.print_game_results()
 
 
 def run_game_with_form():
@@ -39,7 +35,7 @@ def run_game_with_form():
 
 
 if __name__ == '__main__':
-    run_game_with_form()
+    # run_game_with_form()
 
     # print_status()
-    # run_game()
+    run_game()
